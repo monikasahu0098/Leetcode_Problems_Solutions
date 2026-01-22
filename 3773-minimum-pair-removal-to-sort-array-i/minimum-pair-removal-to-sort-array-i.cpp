6034ecb955b3fe82acf0afe0960c1815332d5b6e
Monika Sahu <monikasahu0098@gmail.com>
2026-01-22 09:42:46 +0530

    Time: 0 ms (100.00%) | Memory: 26.5 MB (81.30%) - LeetSync

---
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    int minPairSum(vector<int>&nums){
+        int n=nums.size();
+        int minSum=INT_MAX;
+        int index=-1;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]+nums[i+1]<minSum){
+                index=i;
+                minSum=nums[i]+nums[i+1];
+            }
+        }
+        return index;
+    }
+    int minimumPairRemoval(vector<int>& nums) {
+        int operations=0;
+        while(!is_sorted(begin(nums),end(nums))){
+            int index=minPairSum(nums);
+            nums[index]=nums[index]+nums[index+1];
+            nums.erase(begin(nums)+index+1);
+            operations++;
+        }
+        return operations;
+    }
+};
