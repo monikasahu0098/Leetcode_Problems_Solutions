b8747e7806ae9dbb053e907d533d1dae870ccdf6
Monika Sahu <monikasahu0098@gmail.com>
2026-02-05 08:53:04 +0530

    Time: 4 ms (79.67%) | Memory: 25.8 MB (39.55%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
+    // vector<int> constructTransformedArray(vector<int>& nums) {
+    //     int n=nums.size();
+    //     vector<int>result(n,0);
+    //     for(int i=0;i<n;i++){
+    //         if(nums[i]>0){
+    //             int right=nums[i]%n;
+    //             result[i]=nums[(i+right)%n];
+    //         }else if(nums[i]<0){
+    //             int left=abs(nums[i]%n);
+    //             result[i]=nums[(i-left+n)%n];
+    //         }else{
+    //             result[i]=nums[i];
+    //         }
+    //     }
+    //     return result;
+    // }
     vector<int> constructTransformedArray(vector<int>& nums) {
         int n=nums.size();
         vector<int>result(n,0);
         for(int i=0;i<n;i++){
-            if(nums[i]>0){
-                int right=nums[i]%n;
-                result[i]=nums[(i+right)%n];
-            }else if(nums[i]<0){
-                int left=abs(nums[i]%n);
-                result[i]=nums[(i-left+n)%n];
-            }else{
-                result[i]=nums[i];
+            int shift=nums[i]%n;
+            int newIdx=(i+shift)%n;
+            if(newIdx<0){
+                newIdx=(newIdx+n)%n;
             }
+            result[i]=nums[newIdx];
         }
         return result;
     }
