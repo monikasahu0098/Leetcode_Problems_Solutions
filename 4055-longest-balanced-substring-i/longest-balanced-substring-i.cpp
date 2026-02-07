b9ea4a9a80b9575e0a1cd343c5c705b37590ef66
Monika Sahu <monikasahu0098@gmail.com>
2026-02-07 10:00:31 +0530

    Time: 201 ms (57.10%) | Memory: 25.3 MB (74.49%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isBalanced(vector<int>&f){
+        int same=0;
+        for(int i=0;i<26;i++){
+            if(f[i]==0){
+                continue;
+            }
+            if(same==0){
+                same=f[i];
+            }else if(f[i] != same){
+                return false;
+            }
+        }
+        return true;
+    }
+    int longestBalanced(string s) {
+        int n=s.size();
+        int maxLen=0;
+        for(int i=0;i<n;i++){
+            vector<int>f(26,0);
+            for(int j=i;j<n;j++){
+                f[s[j]-'a']++;
+
+                if(isBalanced(f)){
+                    maxLen=max(maxLen,j-i+1);
+                }
+            }
+        }
+        return maxLen;
+        
+    }
+};
