0a2ee18a99c693fbf3ba43e81654e66e3c29923a
Monika Sahu <monikasahu0098@gmail.com>
2026-02-07 09:42:57 +0530

    Time: 22 ms (64.94%) | Memory: 25.5 MB (72.34%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -47,15 +47,38 @@
 // };
 
 
+// class Solution {
+// public:
+//     int minimumDeletions(string s) {
+//         int n=s.size();
+//         vector<int>right_a(n,0);
+
+//         int counta=0;
+//         for(int i=n-1;i>=0;i--){
+//             right_a[i]=counta;
+//             if(s[i]=='a'){
+//                 counta++;
+//             }
+//         }
+//         int count=INT_MAX;
+//         int countb=0;
+//         for(int i=0;i<n;i++){
+//             count=min(count,countb+right_a[i]);
+//             if(s[i]=='b'){
+//                 countb++;
+//             }
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int minimumDeletions(string s) {
         int n=s.size();
-        vector<int>right_a(n,0);
-
+    
         int counta=0;
         for(int i=n-1;i>=0;i--){
-            right_a[i]=counta;
             if(s[i]=='a'){
                 counta++;
             }
@@ -63,7 +86,10 @@ public:
         int count=INT_MAX;
         int countb=0;
         for(int i=0;i<n;i++){
-            count=min(count,countb+right_a[i]);
+            if(s[i]=='a'){
+                counta--;
+            }
+            count=min(count,countb+counta);
             if(s[i]=='b'){
                 countb++;
             }
