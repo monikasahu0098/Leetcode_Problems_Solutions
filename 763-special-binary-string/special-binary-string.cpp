1e9f37df3a80986ff245a6443ad109b03eaebc0e
Monika Sahu <monikasahu0098@gmail.com>
2026-02-20 09:56:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (80.52%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        string ans="";
+        vector<string>specials;
+        int st=0;
+        int sum=0;
+        for(int i=0;i<s.length();i++){
+            sum+= (s[i]=='1')? +1 :-1;
+            if(sum==0){
+                string inner=s.substr(st+1,i-st-1);
+                specials.push_back("1"+makeLargestSpecial(inner)+"0");
+                st=i+1;
+            }
+        }
+        sort(specials.begin(),specials.end(),greater<string>());
+        for(string &str:specials){
+            ans+=str;
+        }
+        return ans;
+
+    }
+};
