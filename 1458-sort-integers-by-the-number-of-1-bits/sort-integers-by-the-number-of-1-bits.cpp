2f1b46ca0b81463fa5d94d3d1e29405d58331de0
Monika Sahu <monikasahu0098@gmail.com>
2026-02-25 12:08:18 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (79.86%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-    static int countBits(int n){
-        int count=0;
-        while(n != 0){
-            if((n & 1)==1){
-                count++;
-            }
-            n>>=1;
-        }
-        return count;
-    }
+    // static int countBits(int n){
+    //     int count=0;
+    //     while(n != 0){
+    //         if((n & 1)==1){
+    //             count++;
+    //         }
+    //         n>>=1;
+    //     }
+    //     return count;
+    // }
+    // vector<int> sortByBits(vector<int>& arr) {
+    //     sort(arr.begin(),arr.end(), [](int &a,int &b){
+    //         int countA=countBits(a);
+    //         int countB=countBits(b);
+
+    //         if(countA==countB){
+    //             return a<b;
+    //         }
+    //         return countA<countB;
+
+    //     });
+    //     return arr;
+    // }
+
     vector<int> sortByBits(vector<int>& arr) {
         sort(arr.begin(),arr.end(), [](int &a,int &b){
-            int countA=countBits(a);
-            int countB=countBits(b);
+            int countA=__builtin_popcount(a);
+            int countB=__builtin_popcount(b);
 
             if(countA==countB){
                 return a<b;
@@ -23,4 +37,5 @@ public:
         });
         return arr;
     }
+
 };
