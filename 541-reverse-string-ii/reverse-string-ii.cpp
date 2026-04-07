1c548a6e2ed02e95d6a0a094d8385397905588a3
Monika Sahu <monikasahu0098@gmail.com>
2026-04-07 23:08:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (77.07%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n=s.length();
+        for(int i=0;i<n; i+=(2*k)){
+            int l=i;
+            int r=min(i+k-1,n-1);
+
+            while(l<r){
+                swap(s[l],s[r]);
+                l++;
+                r--;
+            }
+        }
+        return s;
+    }
+};
