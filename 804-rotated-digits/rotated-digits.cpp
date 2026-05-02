a3524a42bb8a7cd97d983e2408a44e9c370c57c8
Monika Sahu <monikasahu0098@gmail.com>
2026-05-02 17:24:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (46.67%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -3,18 +3,16 @@ public:
     int rotatedDigits(int n) {
         int count = 0;
         
-        for (int i = 1; i <= n; i++) {
-            int x = i;
-            bool valid = true, changed = false;
-            
-            while (x) {
-                int d = x % 10;
-                
-                if (d == 3 || d == 4 || d == 7) {
-                    valid = false;
+        for(int i=1;i<=n;i++) {
+            int x=i;
+            bool valid=true;
+            bool changed=false;
+            while(x){
+                int d=x%10;
+                if(d==3 || d==4|| d==7){
+                    valid=false;
                     break;
                 }
-                
                 if (d == 2 || d == 5 || d == 6 || d == 9)
                     changed = true;
                 
