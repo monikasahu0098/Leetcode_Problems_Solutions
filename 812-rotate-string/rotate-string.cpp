579e61bb018f39d62a82eae896cebe5a4bbbd5d8
Monika Sahu <monikasahu0098@gmail.com>
2026-05-03 21:42:41 +0530

    Time: 2 ms (12.47%) | Memory: 9 MB (12.66%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        for(int i=0;i<s.length();i++){
+            s=s.substr(1)+s[0];
+            if(s==goal){
+                return true;
+            }
+        }
+        return false;
+    }
+};
