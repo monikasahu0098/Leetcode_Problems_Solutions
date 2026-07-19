e96191ce2f85f76fd08da1005ddd99e815e2dccd
Monika Sahu <monikasahu0098@gmail.com>
2026-07-19 11:16:34 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (60.64%) - LeetSync

--- 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.cpp
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.cpp
@@ -2,29 +2,48 @@ class Solution {
 public:
     int findMiddleIndex(vector<int>& nums) {
         int n=nums.size();
-        vector<int>prefixSum(n,0);
-        prefixSum[0]=nums[0];
-
-        for(int i=1;i<n;i++){
-            prefixSum[i]=prefixSum[i-1]+nums[i];
+        int totalSum=0;
+        for(int x:nums){
+            totalSum+=x;
         }
-
+        int l=0;
         for(int i=0;i<n;i++){
-            int l,r;
-            if(i==0){
-                l=0;
-                r=prefixSum[n-1]-prefixSum[i];
-            }else if(i==n-1){
-                l=prefixSum[i-1];
-                r=0;
-            }else{
-                l=prefixSum[i-1];
-                r=prefixSum[n-1]-prefixSum[i];
-            }
+            int r=totalSum-l-nums[i];
+
             if(l==r){
                 return i;
             }
+            l+=nums[i];
         }
         return -1;
+
+
     }
+    // int findMiddleIndex(vector<int>& nums) {
+        // int n=nums.size();
+        // vector<int>prefixSum(n,0);
+        // prefixSum[0]=nums[0];
+
+        // for(int i=1;i<n;i++){
+        //     prefixSum[i]=prefixSum[i-1]+nums[i];
+        // }
+
+        // for(int i=0;i<n;i++){
+        //     int l,r;
+        //     if(i==0){
+        //         l=0;
+        //         r=prefixSum[n-1]-prefixSum[i];
+        //     }else if(i==n-1){
+        //         l=prefixSum[i-1];
+        //         r=0;
+        //     }else{
+        //         l=prefixSum[i-1];
+        //         r=prefixSum[n-1]-prefixSum[i];
+        //     }
+        //     if(l==r){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+    // }
 };
