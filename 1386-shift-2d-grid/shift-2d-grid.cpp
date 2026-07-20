5769853308f15ddbe46794500878f677df8967fe
Monika Sahu <monikasahu0098@gmail.com>
2026-07-20 22:25:35 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (66.35%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -6,23 +6,21 @@ public:
         int n=r*c;
 
         k=k%n;
+        if(!k){
+            return grid;
+        }
 
-        if(!k){ return grid; }
-
-        auto shift=[&](int i,int j){
-            while(i<j){
-                swap(grid[i/c][i%c], grid[j/c][j%c]);
-                i++;
-                j--;
+        auto shift=[&](int a,int b){
+            while(a<b){
+                swap(grid[a/c][a%c],grid[b/c][b%c]);
+                a++;
+                b--;
             }
         };
-        
         shift(0,n-1);
         shift(0,k-1);
         shift(k,n-1);
 
         return grid;
-
-
     }
 };
