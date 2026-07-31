e5a69d497813a904ddb5e2d101c112230c091995
Monika Sahu <monikasahu0098@gmail.com>
2026-07-31 15:20:11 +0530

    Time: 3 ms (34.13%) | Memory: 10.4 MB (51.68%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     string reverseWords(string s) {
         int n=s.size();
-        reverse(s.begin(),s.end());
-
         string ans="";
         
+        reverse(s.begin(),s.end());
+
         for(int i=0;i<n;i++){
             string word="";
             while(i<n && s[i] !=' '){
@@ -13,8 +13,8 @@ public:
                 i++;
             }
             reverse(word.begin(),word.end());
-            if(word.length()>0){
-                ans+=" "+word;
+            if(word.size()>0){
+                ans+= " "+word;
             }
         }
         return ans.substr(1);
