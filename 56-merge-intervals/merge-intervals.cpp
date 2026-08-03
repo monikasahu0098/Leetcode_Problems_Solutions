6cfe55f62044c82abd545625a33cbd03f2a9ab6b
Monika Sahu <monikasahu0098@gmail.com>
2026-08-03 12:36:04 +0530

    Time: 4 ms (65.94%) | Memory: 23.7 MB (82.59%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,25 +1,51 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>>result;
-        int n=intervals.size();
+    // vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    //     vector<vector<int>>result;
+    //     int n=intervals.size();
 
-        sort(intervals.begin(),intervals.end());
+    //     sort(intervals.begin(),intervals.end());
+    //     for(int i=0;i<n;i++){
+    //         int st=intervals[i][0];
+    //         int end=intervals[i][1];
+    //         if(!result.empty() && end<=result.back()[1]){
+    //             continue;
+    //         }
+    //         for(int j=i+1;j<n;j++){
+    //             if(intervals[j][0]<=end){
+    //                 end=max(end,intervals[j][1]);
+    //             }else{
+    //                 break;
+    //             }
+    //         }
+    //         result.push_back({st,end});
+    //     }
+    //     return result;
+    // }
+
+
+    vector<vector<int>> merge(vector<vector<int>>& inter) {
+        vector<vector<int>>ans;
+        int n=inter.size();
+
+        sort(inter.begin(),inter.end());
+
         for(int i=0;i<n;i++){
-            int st=intervals[i][0];
-            int end=intervals[i][1];
-            if(!result.empty() && end<=result.back()[1]){
+            int st=inter[i][0];
+            int end=inter[i][1];
+
+            if(!ans.empty() && end<=ans.back()[1]){
                 continue;
             }
             for(int j=i+1;j<n;j++){
-                if(intervals[j][0]<=end){
-                    end=max(end,intervals[j][1]);
-                }else{
+                if(end>=inter[j][0]){
+                    end=max(end,inter[j][1]);
+                }else {
                     break;
                 }
             }
-            result.push_back({st,end});
+            ans.push_back({st,end});
         }
-        return result;
+        return ans;
     }
 };
